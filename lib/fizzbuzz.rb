12a11722e807453f61ac2d52587a20a3a019b997
Student <Student@Admins-MacBook-Air.local>
2019-09-23 16:13:08 +0100

    finished buzz and fizbuzz conditions

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,11 @@
-def fizzbuzz
+def fizzbuzz(number)
+ 'fizz'
+
+  if number % 3 == 0 and number % 5 == 0
+    puts 'fizzbuzz'
+  elsif number % 5 == 0
+    puts 'buzz'
+  elsif number % 3 == 0
+  puts 'fizz'
+  end
 end
